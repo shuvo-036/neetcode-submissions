e05ae7418d9f5e5c76d12d3a6bd3800de5cf336a
Golam Moniruzzaman <golammoniruzzaman036@gmail.com>
2026-09-18 22:49:47 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        res =[]
+
+        def solve(start, path):
+            if len(path) == k:
+                res.append(path[:])
+                return
+            
+            for i in range(start , n+1):
+                path.append(i)
+                solve(i +1 , path)
+                path.pop()
+
+        solve(1, [])
+        return res
