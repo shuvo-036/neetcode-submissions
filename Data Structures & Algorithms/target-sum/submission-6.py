821331f443610bf61cf96122be0aa0e60d742782
Golam Moniruzzaman <golammoniruzzaman036@gmail.com>
2026-09-22 20:02:48 +0530

    Add: target-sum - submission-6

---
+++ Data Structures & Algorithms/target-sum/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+        n= len(nums)
+
+        def solve(i, curr_some):
+
+            if i == n:
+                if curr_some == target:
+                    return 1
+                else:
+                    return 0
+                
+            
+            
+
+            
+            take = solve(i+1, curr_some +nums[i])
+            
+            not_take = solve(i+1, curr_some - nums[i])
+
+            return take + not_take
+        
+        return solve(0,0)
